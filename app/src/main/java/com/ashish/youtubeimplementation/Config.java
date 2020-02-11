72082192e975e915c5d41507ef4ef9741fb058f0
ashitests <ashishtests07@gmail.com>
2020-02-11 12:36:10 +0530

    Initial commit

---
+++ app/src/main/java/com/ashish/youtubeimplementation/Config.java
@@ -0,0 +1,8 @@
+package com.ashish.youtubeimplementation;
+
+public class Config {
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCIiflwvu-8aVQEeyPTelsO-ZdidOSPQBg";
+}
